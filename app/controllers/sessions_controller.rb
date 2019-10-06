b000715a7df381aa7f68edc515f6d9c437590e8b
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-07 01:22:02 +0900

    signin / cover page completed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to feeds_path
+      flash[:notice] = "ログインしました"
+    else
+      flash.now[:danger] = "メールアドレスまたはパスワードが違います"
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = "ログアウトしました"
+    redirect_to new_session_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'users#welcome'
 end
