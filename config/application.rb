16c8a39c9e0e4043d1623ac6c35880cb3e11a42c
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-08 00:45:43 +0900

    timezone setting and patically translation

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,7 @@ module Instashot
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
 end
