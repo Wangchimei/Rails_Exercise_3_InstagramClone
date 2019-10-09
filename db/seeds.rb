a3cbdbf79aa2691ab3407ce53a2efb8396522c4b
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-09 20:00:13 +0900

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# users dummy data
+60.times do |user|
+  name = Faker::Name.unique.name
+  email = Faker::Internet.safe_email(name: name)
+  password = "testtest"
+  avatar = Faker::LoremFlickr.image(size: "400x400", search_terms: ['animal']) 
+  date = Faker::Date.backward(days: 400)
+  User.create!(name: name, 
+              email: email, 
+              password: password, 
+              created_at: date,
+              avatar: avatar)
+end
+
+# feed dummy data
+60.times do |feed|
+  image = Faker::LoremFlickr.image(size: "600x600")
+  content = Faker::Lorem.paragraph(sentence_count: 2)
+  date = Faker::Date.backward(days: 400)
+  Feed.create!(name: name, 
+              email: email, 
+              password: password, 
+              created_at: date,
+              user_id: rand(1..60))
+end
+
+# comments dummy data
+200.times do |comment|
+  content = Faker::Lorem.paragraph(sentence_count: 3)
+  date = Faker::Date.backward(days: 200)
+  Comment.create!(content: content,
+                  created_at: date,
+                  feed_id: rand(1..60),
+                  user_id: rand(1..60))
+end
+
+# favorites dummy data
+120.times do |favorite|
+  date = date = Faker::Date.backward(days: 100)
+  Favorite.create!(feed_id: rand(1..60),
+                  user_id: rand(1..60))
+end
+
